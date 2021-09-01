1d986d7dd2c463236e4e6bab27b2f1798934a7c8
jesus re <eisa.eisa.r.r@gmail.com>
2021-09-01 16:43:07 +0430

    deploy

--- src/components/Profile/styles.js
+++ src/components/Profile/styles.js
@@ -174,7 +174,7 @@ export const StyledProfilePostsCtImages = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
-  justify-content: space-evenly;
+  justify-content: flex-start;
   flex-wrap: wrap;
 `
 export const StyledProfilePageSinglePost = styled.div`
