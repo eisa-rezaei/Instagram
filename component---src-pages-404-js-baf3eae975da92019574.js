5ffaf9783f5c8d7e21ebbd6d2bf5b51df3314360
eisa-rezaei <78264204+eisa-rezaei@users.noreply.github.com>
2021-09-02 13:24:18 +0000

    Deploying to gh-pages from  @ 7d5420ba1801af71498509da9d22af6b0fe38e08 🚀

---
+++ component---src-pages-404-js-baf3eae975da92019574.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,l){l.r(t);var n=l(5444),r=l(7294),a=l(8065);t.default=function(){return r.createElement(a.Z,null,r.createElement("h2",null," wait a minute !! "),r.createElement("h4",null,"Oh!, It seems something went wrong!"),r.createElement(n.rU,{to:"/"},"Back to home"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-baf3eae975da92019574.js.map
